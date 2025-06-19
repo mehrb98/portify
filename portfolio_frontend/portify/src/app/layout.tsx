8e0f293317742586b2876349501d813510556039
Mehrulloh Boboev <mehrb98@gmail.com>
2025-06-19 05:18:17 +0200

    Fix provider

--- portfolio_frontend/portify/src/app/layout.tsx
+++ portfolio_frontend/portify/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { UIProvider } from "@/providers/UIProvider";
+import { HeroUIProvider } from '@heroui/react'
 
 export const metadata: Metadata = {
 	title: "Portify",
@@ -15,9 +15,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
 			<body className="min-h-screen bg-gray-900">
-				<UIProvider>
+				<HeroUIProvider>
 					{children}
-				</UIProvider>
+				</HeroUIProvider>
 			</body>
 		</html>
 	);
