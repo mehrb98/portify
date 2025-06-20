df31a2f59f0a6284210cbeb9cc2a2fb0750d85fa
Mehrulloh Boboev <mehrb98@gmail.com>
2025-06-21 01:13:37 +0200

    Fix param type

--- portfolio_frontend/portify/src/app/project/[id]/page.tsx
+++ portfolio_frontend/portify/src/app/project/[id]/page.tsx
@@ -2,15 +2,9 @@ import { notFound } from "next/navigation";
 import { projects } from "@/data/projects";
 import { ProjectContent } from "@/components/Project/ProjectContent";
 
-type TPage = {
-   params: { id: string }
-};
-
-export default async function Page({ params }: TPage) {
-   console.log("Project->", projects)
-
-   const projectId = parseInt(params.id);
-   const project = projects.find(p => p.id === projectId);
+export default async function Page({ params }: { params: { id: string } }) {
+   const projectId = params.id;
+   const project = projects.find(p => p.id.toString() === projectId);
 
    if (!project) return notFound();
 
